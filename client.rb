7f87483ef39d1c204a1fdbfdd5611f1b9b591844
agrathwohl <agrathwohl@protonmail.ch>
2015-07-26 17:06:53 -0400

    ClientKnob accepts file upload and reports the final JSON message on the file

--- client.rb
+++ client.rb
@@ -2,13 +2,26 @@
 require "rubygems"
 require "bundler/setup"
 require "sinatra"
+require "tempfile"
+require "json"
+require "./lib/knob"
 
 get "/upload" do
   haml :upload
 end
 
 post "/upload" do
-  # ...
+  begin
+    path = Pathname.new(params['myfile'][:tempfile])
+    k    = Knob.new("#{path.dirname}/#{path.basename}")
+    msg  = k.scan
+  rescue Exception => e
+    puts e
+    retry while tries < 5
+  ensure
+    k = nil
+  end
+  msg
 end
 
 get "/hi" do
