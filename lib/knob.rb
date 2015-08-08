0316802e997c9a9bd38be7112026c7755a0c97ef
agrathwohl <agrathwohl@protonmail.ch>
2015-08-08 15:30:44 -0400

    knob picks up encoding data with a new array which allows for iteration over several soxi commands; this will allow for user configuration of checks (todo)

--- lib/knob.rb
+++ lib/knob.rb
@@ -9,6 +9,7 @@ class Knob
     @file_score = 0
     @levelvals  = {:flat => 1.0, :crest => 6.0, :peak => -3.0, :rmsl => -34.0, :rmsh => -20.0}
     @encodevals = {:sampleEnc => [16,24], :sampleDep => [16,24], :sampleRate => [44100,48000,96000], :channels => [2], :length => 7199}
+    @issues     = []
   end
   attr_reader :file, :file_path, :file_score
   def scan
@@ -17,26 +18,31 @@ class Knob
         false   elsif @file_path.extname    =~ /\.[flac|aif|aiff|wav|raw]+/
         true
     end
-    encCmd       = `soxi "#{@file_path}"`
-    @sampleEnc   = "#{encCmd.split("\n")[-1]}".match(/\d+/)[0].to_i     # sample encoding
-    @sampleDep   = "#{encCmd.split("\n")[-5]}".match(/\d+/)[0].to_i     # sample bit depth
-    @sampleRate  = "#{encCmd.split("\n")[-6]}".match(/\d+/)[0].to_i     # sampling frequency
-    @channels    = "#{encCmd.split("\n")[-7]}".match(/\d+/)[0].to_i     # channel count
-    @enc = {:sample_encoding => @sampleEnc, :sample_depth => @sampleDep, 
-     :sample_rate => @sampleRate, :channels => @channels}
+    counter      = 1
+    soxi_checks  = ["-t","-r","-c","-D","-b"]
+    encCmd       = []
+    soxi_checks.each do |cmd|
+      encCmd[counter] = `soxi #{cmd} "#{@file_path}"`
+      puts "#{counter}: #{encCmd[counter]}"
+      counter += 1
+    end
+    @enc = {:sample_encoding => "#{encCmd[5]}".to_i, :sample_depth => "#{encCmd[5]}".to_i, 
+     :sample_rate => "#{encCmd[2]}".to_i, :channels => "#{encCmd[3]}".to_i, :lossless => @lossless}
     # Audio dynamics measurement scanning
     statsCmd = `sox "#{@file_path}" -n stats 2>&1`
+    @seconds = "#{statsCmd.split("\n")[-3]}".match(/\d+.\d+/)[0].to_f   # length in seconds
     @flat    = "#{statsCmd.split("\n")[-7]}".match(/\d+.\d+/)[0].to_f   # number of samples that hit 0dBFS
     @crest   = "#{statsCmd.split("\n")[-8]}".match(/\d+.\d+/)[0].to_f   # peak-to-RMS ratio
-    @peak    = "#{statsCmd.split("\n")[-12]}".match(/-\d+.\d+/)[0].to_f # loudest measured sample in the file
-    @seconds = "#{statsCmd.split("\n")[-3]}".match(/\d+.\d+/)[0].to_f   # length in seconds
     @rms     = "#{statsCmd.split("\n")[-11]}".match(/-\d+.\d+/)[0].to_f # look it up on wikipedia
+    @peak    = "#{statsCmd.split("\n")[-12]}".match(/-\d+.\d+/)[0].to_f # loudest measured sample in the file
+    @stats = {:flat => @flat, :crest => @crest, :peak => @peak, :rms => @rms,
+      :seconds => @seconds}
     # Warnings
     return "bad_length" if "#{@seconds}".to_f > @encodevals[:length]
     score
   end
   attr_reader :sampleEnc, :sampleDep, :sampleRate, :channels, :lossless
-  attr_reader :flat, :crest, :peak, :rms, :seconds
+  attr_reader :flat, :crest, :peak, :rms, :seconds, :issues
   def score # TODO: Add a check for top/tail padding; add 15 to score if both are adaquate. 5 if only one.
     if @lossless == true # If the file isn't lossless, there's literally no reason to master it.
     # Audio encoding compliance scoring
@@ -46,11 +52,22 @@ class Knob
         @encodevals[:sampleDep].any?  {|dep| dep == "#{@sampleDep}".to_i}    == true &&
        	@encodevals[:channels].any?   {|chan| chan == "#{@channels}".to_i}   == true
     # Audio dynamics measurement scoring
-      @file_score += 10 if "#{@flat}".to_f  < @levelvals[:flat]
-      @file_score += 5  if "#{@rms}".to_f   < @levelvals[:rmsh] && "#{@rms}".to_f > @levelvals[:rmsl]
-      @file_score += 10 if "#{@peak}".to_f  < @levelvals[:peak]
-      @file_score += 5  if "#{@crest}".to_f > @levelvals[:crest]
+      @file_score   += 10 if "#{@flat}".to_f  < @levelvals[:flat]
+      @file_score   += 10 if "#{@peak}".to_f  < @levelvals[:peak]
+      @file_score   += 5  if "#{@crest}".to_f > @levelvals[:crest]
+      if @rms.between?(@levelvals[:rmsl],@levelvals[:rmsh])
+        @file_score += 15
+      elsif @rms < @levelvals[:rmsh]
+        @file_score += 10
+      elsif @rms < @levelvals[:rmsl]
+        @file_score += 5
+      else
+        @issues.push("rms")
+      end
+    else 
+      @file_score = 0
     end
-    return {:file => "#{@file_path}", :score => @file_score}.to_json
+    return {:file => "#{@file_path}", :score => @file_score,
+            :enc  => @enc, :stats => @stats}.to_json
   end
 end
