e42951d32a5b48ba7b7bb7815c7d68b09271973c
Andrew Grathwohl <andrew@grathwohl.me>
2016-03-06 10:26:41 -0500

    cleaned up audio mastering methods, removed needless messaging

--- lib/master.rb
+++ lib/master.rb
@@ -11,6 +11,7 @@ class MasterKnob
     end
     def analyze
         @steps = {}
+	# Set values for each processing step based on file analysis
         if @stats["peak"] > -3.0
             if @stats["rms"] > -16.0
                 @steps["gain"]  = -6
@@ -27,6 +28,7 @@ class MasterKnob
         puts "SoX arguments: #{@steps}"
     end
     def construct1
+	# High-pass at 50Hz, low-pass at 14kHz
         sinc1 = "sinc 50-14k" # high-pass at 50Hz, low-pass at 14kHz
         sox_cmd1 = "sox #{@audio_path.realpath} #{@audio_path.realpath}.lev.wav #{sinc1} "
         if @steps["gain"].nil? == false
@@ -36,19 +38,20 @@ class MasterKnob
         elsif @steps["norm"].nil? == false
             sox_cmd1 += "norm #{@steps["norm"]}"
         end
-        puts sox_cmd1
         `#{sox_cmd1}`
         puts "SoX CMD 1 - Complete"
     end
     def construct2
+	# Run SoX compander on leveled audio file
         sox_cmd2 = "sox #{@audio_path.realpath}.lev.wav #{@audio_path.realpath}.fix.wav gain -h "
         drc = @stats["rms"] || "-20"
         sox_cmd2 += "compand 0.02,0.2 6:-40,-30,#{drc} -10 -6 0.2 norm 0"
-        puts sox_cmd2
         `#{sox_cmd2}`
         puts "SoX CMD 2 - Complete"
+	# Check the crest factor for the resulting file
         crest_check = `sox #{@audio_path.realpath}.fix.wav -n stats 2>&1`
         crest_now   = crest_check.split("\n")[-8].match(/\d+.\d+/)[0].to_f
+	# Process the file again if it doesn't meet spec
         if crest_now > 7.5
             crest_fix = crest_now - 6.5
             puts "Crest is low, running a final gain boost."
